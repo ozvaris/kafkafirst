1813f2f9915f660ea38a68c36185af381908549e
Ozgur <ozgurozvaris@hotmail.com>
2022-04-04 00:55:38 +0100

    hello word example completed

--- kafka-basics/src/main/java/io/brainpark/demos/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/io/brainpark/demos/kafka/ProducerDemo.java
@@ -1,6 +1,8 @@
 package io.brainpark.demos.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,12 +20,22 @@ public class ProducerDemo {
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "");
-        // create the Producer
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
-        // send data
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // create a producer record
+        ProducerRecord<String, String> producerRecord =
+                new ProducerRecord<>("demo_java", "hello world");
+        // send the data - asynchronous
+        producer.send(producerRecord);
+
+        //flush data - synchronous
+        producer.flush();
 
         // flush and close the Producer
+        producer.close();
 
 
     }
